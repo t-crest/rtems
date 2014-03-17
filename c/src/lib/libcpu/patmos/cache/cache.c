fa20653052880e3b6f28a273dd5c2babd7b3b6ba
afpr <afpr@gmv.com>
2014-03-17 10:52:05 +0000

    Removed flush instruction from inline asm

--- c/src/lib/libcpu/patmos/cache/cache.c
+++ c/src/lib/libcpu/patmos/cache/cache.c
@@ -1,7 +1,7 @@
 /*
- *  Cache Management Support Routines for the PATMOS
+ *  Cache Management Support Routines for Patmos
  *
- *  $Id: cache.c,v 1.00 2013/06/14 15:17:12 André Rocha $
+ *  $Id: cache.c,v 1.00 2013/06/14 15:17:12 Andre Rocha $
  */
 
 #include <rtems.h>
@@ -10,12 +10,11 @@
 
 #if defined(HAS_INSTRUCTION_CACHE)
 
+/* XXX these need to be addressed */
 void _CPU_cache_invalidate_entire_instruction ( void )
 {
-  asm volatile ("flush");
 }
 
-/* XXX these need to be addressed */
 void _CPU_cache_freeze_instruction ( void )
 {
 }
